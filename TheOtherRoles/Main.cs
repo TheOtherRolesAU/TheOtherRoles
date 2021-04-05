6487d4986e4711f0846bc7f6e5e86687f8131626
Eisbison <eisbison.creator@gmail.com>
2021-04-05 18:40:01 +0200

    Remove server code to enable 3rd party tools

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -110,22 +110,9 @@ namespace TheOtherRoles
         public static int optionsPage = 1;
 
         public static ConfigEntry<bool> DebugMode { get; private set; }
-        public ConfigEntry<string> Ip { get; set; }
-        public ConfigEntry<ushort> Port { get; set; }
 
         public override void Load() {
             DebugMode  = Config.Bind("Custom", "Enable Debug Mode", false);
-            Ip = Config.Bind("Custom", "Ipv4 or Hostname", "127.0.0.1");
-            Port = Config.Bind("Custom", "Port", (ushort)22023);
-
-            IRegionInfo customRegion = new DnsRegionInfo(Ip.Value, "Custom", StringNames.NoTranslation, Ip.Value, Port.Value).Cast<IRegionInfo>();
-            ServerManager serverManager = DestroyableSingleton<ServerManager>.CMJOLNCMAPD;
-            IRegionInfo[] regions = ServerManager.DefaultRegions;
-
-            regions = regions.Concat(new IRegionInfo[] { customRegion }).ToArray();
-            ServerManager.DefaultRegions = regions;
-            serverManager.GDOLGIJJLBL = regions;
-            serverManager.SaveServers();
 
             Harmony.PatchAll();
         }
