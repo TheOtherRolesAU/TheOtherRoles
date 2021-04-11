2e9829ebec584b131c980cdb75c5c9aaae76c112
Eisbison <eisbison.creator@gmail.com>
2021-04-11 12:56:31 +0200

    Add missing patch

--- TheOtherRoles/PlayerControlPatch.cs
+++ TheOtherRoles/PlayerControlPatch.cs
@@ -473,4 +473,20 @@ namespace TheOtherRoles {
             }
         }
     }
+
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.AMDJMEEHNIG), MethodType.Getter)]
+    class PlayerControlCanMovePatch {
+        public static bool Prefix(PlayerControl __instance, ref bool __result)
+        {
+            __result = __instance.moveable &&
+                !Minigame.Instance &&
+                (!DestroyableSingleton<HudManager>.JECNDKBIOFO || (!DestroyableSingleton<HudManager>.CMJOLNCMAPD.Chat.LFGAAGECFFO && !DestroyableSingleton<HudManager>.CMJOLNCMAPD.KillOverlay.LFGAAGECFFO && !DestroyableSingleton<HudManager>.CMJOLNCMAPD.GameMenu.LFGAAGECFFO)) &&
+                (!MapBehaviour.Instance || !MapBehaviour.Instance.IPOGMCKNALI) &&
+                !MeetingHud.Instance &&
+                !CustomPlayerMenu.Instance &&
+                !ExileController.Instance &&
+                !IntroCutscene.Instance;
+            return false;
+        }
+    }
 }
