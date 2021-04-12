a8850d1af1b0ccd4333459308d9ace7440a12b72
Eisbison <eisbison.creator@gmail.com>
2021-04-12 12:31:18 +0200

    Update buttons on menu start

--- TheOtherRoles/CrowdedPatches.cs
+++ TheOtherRoles/CrowdedPatches.cs
@@ -9,56 +9,61 @@ using SaveManager = ALOOOIHKCAC;
 using GameOptionsData = IGDMNKLDEPI;
 
 namespace TheOtherRoles {
-
-    [HarmonyPatch(typeof(CreateOptionsPicker), nameof(CreateOptionsPicker.Start))]
-    public static class CreateOptionsPickerStartPatch {
+    [HarmonyPatch]
+    static class CreateOptionsPickerPatch {
         public static List<SpriteRenderer> additionalButtons = new List<SpriteRenderer>();
 
-        public static void Postfix(CreateOptionsPicker __instance) {
-            List<SpriteRenderer> maxPlayerButtons = __instance.MaxPlayerButtons.ToList();
-            additionalButtons = new List<SpriteRenderer>();
+        [HarmonyPatch(typeof(CreateOptionsPicker), nameof(CreateOptionsPicker.Start))]
+        public static class CreateOptionsPickerStartPatch {
+            public static void Postfix(CreateOptionsPicker __instance) {
+                List<SpriteRenderer> maxPlayerButtons = __instance.MaxPlayerButtons.ToList();
+                additionalButtons = new List<SpriteRenderer>();
 
-            for (int i = 1; i < 6; i++) {
-                SpriteRenderer nextButton = Object.Instantiate(maxPlayerButtons.Last(), maxPlayerButtons.Last().transform.parent);
-                additionalButtons.Add(nextButton);
-                nextButton.enabled = false;
-                nextButton.gameObject.name = "1" + i;
-                TextRenderer text = nextButton.gameObject.GetComponentInChildren<TextRenderer>();
-                text.render = null;
-                nextButton.GetComponentInChildren<MeshFilter>().mesh = null;
-                text.Text = "1" + i;
-                text.RefreshMesh();
-                text.Color = Helpers.isCustomServer() ? Color.white : Palette.JMELLHINKGM;
+                for (int i = 1; i < 6; i++) {
+                    SpriteRenderer nextButton = Object.Instantiate(maxPlayerButtons.Last(), maxPlayerButtons.Last().transform.parent);
+                    additionalButtons.Add(nextButton);
+                    nextButton.enabled = false;
+                    nextButton.gameObject.name = "1" + i;
+                    TextRenderer text = nextButton.gameObject.GetComponentInChildren<TextRenderer>();
+                    text.render = null;
+                    nextButton.GetComponentInChildren<MeshFilter>().mesh = null;
+                    text.Text = "1" + i;
+                    text.RefreshMesh();
+                    text.Color = Helpers.isCustomServer() ? Color.white : Palette.JMELLHINKGM;
 
-                nextButton.transform.position = nextButton.transform.position + new Vector3(i * (maxPlayerButtons[1].transform.position.x - maxPlayerButtons[0].transform.position.x), 0, 0);
-                var passiveButton = nextButton.GetComponent<PassiveButton>();
-                passiveButton.OnClick.RemoveAllListeners();
+                    nextButton.transform.position = nextButton.transform.position + new Vector3(i * (maxPlayerButtons[1].transform.position.x - maxPlayerButtons[0].transform.position.x), 0, 0);
+                    var passiveButton = nextButton.GetComponent<PassiveButton>();
+                    passiveButton.OnClick.RemoveAllListeners();
 
-                void onClick() {
-                    bool isCustom = Helpers.isCustomServer();
+                    void onClick() {
+                        if (!Helpers.isCustomServer()) return;
 
-                    foreach (SpriteRenderer renderer in additionalButtons) {
-                        renderer.enabled = false;
-                        renderer.gameObject.GetComponentInChildren<TextRenderer>().Color = isCustom ? Color.white : Palette.JMELLHINKGM;
+                        nextButton.enabled = true;                    
+
+                        byte value = byte.Parse(nextButton.name);
+                        var targetOptions = __instance.GetTargetOptions();
+                        if (value <= targetOptions.PCBBPGNJPJN) {
+                            targetOptions.PCBBPGNJPJN = value - 1;
+                            __instance.ABNOBIOJDEH(targetOptions.PCBBPGNJPJN);
+                        }
+                        __instance.SetMaxPlayersButtons(value);
                     }
-
-                    if (!isCustom) return;
-
-                    nextButton.enabled = true;                    
-
-                    byte value = byte.Parse(nextButton.name);
-                    var targetOptions = __instance.GetTargetOptions();
-                    if (value <= targetOptions.PCBBPGNJPJN) {
-                        targetOptions.PCBBPGNJPJN = value - 1;
-                        __instance.ABNOBIOJDEH(targetOptions.PCBBPGNJPJN);
-                    }
-                    __instance.SetMaxPlayersButtons(value);
+                    passiveButton.OnClick.AddListener((UnityEngine.Events.UnityAction)onClick);
                 }
-                passiveButton.OnClick.AddListener((UnityEngine.Events.UnityAction)onClick);
+            }
+        }
+
+        [HarmonyPatch(typeof(HostGameButton), nameof(HostGameButton.OnClick))]
+        class HostGameButtonOnClickPatch {
+            public static void Postfix() {
+                bool isCustom = Helpers.isCustomServer();
+                foreach (SpriteRenderer renderer in additionalButtons) {
+                    renderer.enabled = false;
+                    renderer.gameObject.GetComponentInChildren<TextRenderer>().Color = isCustom ? Color.white : Palette.JMELLHINKGM;
+                }
             }
         }
     }
-
 
     [HarmonyPatch(typeof(SaveManager), nameof(SaveManager.GJJLFHMDLGA), MethodType.Getter)]
     public static class SaveManagerGetHostOptions
