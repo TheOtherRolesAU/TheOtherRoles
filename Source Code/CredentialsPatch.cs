f45aa7afc4c6258351c1ab7c1fe0f0d6f2919b45
Eisbison <eisbison.creator@gmail.com>
2021-04-15 16:40:17 +0200

    v2.2.2

--- Source Code/CredentialsPatch.cs
+++ Source Code/CredentialsPatch.cs
@@ -14,19 +14,20 @@ namespace TheOtherRoles
         private static class VersionShowerPatch
         {
             static void Postfix(VersionShower __instance) {
-                string spacer = new String('\n', 21);
+                string spacer = new String('\n', 8);
                 string fullCredentials = $"<color=#FCCE03FF>TheOtherRoles</color> v{TheOtherRolesPlugin.Major}.{TheOtherRolesPlugin.Minor}.{TheOtherRolesPlugin.Patch}:\n- Modded by <color=#FCCE03FF>Eisbison</color> and <color=#FFEB91FF>Thunderstorm584</color>\n- Balanced with <color=#FFEB91FF>Dhalucard</color>\n- Button design by <color=#FFEB91FF>Bavari</color>";
                 if (__instance.text.text.Contains(spacer))
-                    __instance.text.text += "\n" + fullCredentials;
+                    __instance.text.text = __instance.text.text + "\n" + fullCredentials;
                 else
-                    __instance.text.text += spacer + fullCredentials;
+                    __instance.text.text = __instance.text.text + spacer + fullCredentials;
+                __instance.text.alignment = TMPro.TextAlignmentOptions.TopLeft;
             }
         }
 
         [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
         private static class PingTrackerPatch
         {
-            static void Postfix(VersionShower __instance)
+            static void Postfix(PingTracker __instance)
             {
                 if (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GCDONLGCMIL.Started)
                     __instance.text.text += "\n<color=#FCCE03FF>TheOtherRoles</color>\nModded by <color=#FCCE03FF>Eisbison</color>";
