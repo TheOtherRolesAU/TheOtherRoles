79f7f3d66a1c0654095266931a98fd37cfe45c19
Eisbison <eisbison.creator@gmail.com>
2021-04-18 23:59:32 +0200

    Remove animation reverse

--- TheOtherRoles/JackInTheBox.cs
+++ TheOtherRoles/JackInTheBox.cs
@@ -27,16 +27,15 @@ namespace TheOtherRoles{
             return boxAnimationSprites;
         }
 
-        public static void startAnimation(int ventId, bool IsOpening) {
+        public static void startAnimation(int ventId) {
             JackInTheBox box = AllJackInTheBoxes.FirstOrDefault((x) => x?.vent != null && x.vent.Id == ventId);
             if (box == null) return;
             Vent vent = box.vent;
-            bool isOpenAnim = IsOpening;
 
             HudManager.CHNDKKBEIDG.StartCoroutine(Effects.DCHLMIDMBHG(0.6f, new Action<float>((p) => {
                 var sprites = getBoxAnimationSprites();
                 if (vent != null && vent.KJAENOGGEOK != null && sprites != null && sprites.Count > 0) {
-                    int index = isOpenAnim ? (int)(p * sprites.Count) : sprites.Count - (int)(p * sprites.Count);
+                    int index = (int)(p * sprites.Count);
                     vent.KJAENOGGEOK.sprite = sprites[Mathf.Clamp(index, 0, sprites.Count-1)];
                     if (p == 1f) vent.KJAENOGGEOK.sprite = getBoxSprite();
                 }
--- TheOtherRoles/UsablesPatch.cs
+++ TheOtherRoles/UsablesPatch.cs
@@ -83,7 +83,7 @@ namespace TheOtherRoles
 
             bool isEnter = !PlayerControl.LocalPlayer.inVent;
 
-            JackInTheBox.startAnimation(__instance.Id, isEnter);
+            JackInTheBox.startAnimation(__instance.Id);
             __instance.SetButtons(isEnter);
             MessageWriter writer = AmongUsClient.Instance.StartRpc(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.UseUncheckedVent, Hazel.SendOption.Reliable);
             writer.WritePacked(__instance.Id);
