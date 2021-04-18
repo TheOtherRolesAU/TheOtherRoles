d0bf03125df5e7e8bea8dff7c3196a2c9ab8d85e
Eisbison <eisbison.creator@gmail.com>
2021-04-19 00:56:12 +0200

    Fix offset

--- TheOtherRoles/JackInTheBox.cs
+++ TheOtherRoles/JackInTheBox.cs
@@ -47,9 +47,8 @@ namespace TheOtherRoles{
 
         public JackInTheBox(Vector2 p) {
             gameObject = new GameObject("JackInTheBox");
-            Vector3 ventOffset = new Vector3(0f, 0.25f, 0f);
             Vector3 position = new Vector3(p.x, p.y, PlayerControl.LocalPlayer.transform.position.z + 1f); 
-            position += (Vector3)PlayerControl.LocalPlayer.Collider.offset - ventOffset; // Add collider offset and vent offset, such that DoMove and WalkPlayerTo end up at a valid position
+            position += (Vector3)PlayerControl.LocalPlayer.Collider.offset; // Add collider offset that DoMove moves the player up at a valid position
             // Create the marker
             gameObject.transform.position = position;
             var boxRenderer = gameObject.AddComponent<SpriteRenderer>();
@@ -64,7 +63,7 @@ namespace TheOtherRoles{
             vent.Center = null;
             vent.EnterVentAnim = null;
             vent.ExitVentAnim = null;
-            vent.Offset = ventOffset;
+            vent.Offset = new Vector3(0f, 0.25f, 0f);
             vent.GetComponent<PowerTools.SpriteAnim>()?.Stop();
             vent.Id = ShipStatus.Instance.GJHKPDGJHJN.Select(x => x.Id).Max() + 1; // Make sure we have a unique id
             var ventRenderer = vent.GetComponent<SpriteRenderer>();
