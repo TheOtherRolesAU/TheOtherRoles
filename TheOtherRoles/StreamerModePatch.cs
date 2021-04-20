75d1aa9a7c6c0f8b57dd9c8d119dff64de82fb46
Eisbison <eisbison.creator@gmail.com>
2021-04-20 11:18:00 +0200

    Hide text in ip/port/code field

--- TheOtherRoles/StreamerModePatch.cs
+++ TheOtherRoles/StreamerModePatch.cs
@@ -38,4 +38,14 @@ namespace TheOtherRoles {
             }
         }
     }
+
+    [HarmonyPatch(typeof(TextBoxTMP), nameof(TextBoxTMP.SetText))]
+	public static class HiddenTextPatch
+	{
+		private static void Postfix(TextBoxTMP __instance)
+		{
+			bool flag = TheOtherRolesPlugin.StreamerMode.Value && (__instance.name == "GameIdText" || __instance.name == "IpTextBox" || __instance.name == "PortTextBox");
+			if (flag) __instance.outputText.text = new string('*', __instance.text.Length);
+		}
+	}
 }
