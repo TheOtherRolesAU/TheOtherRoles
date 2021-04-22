9e50fb790d346dfd32a4e4c17825b9f7e95cf4d4
Eisbison <eisbison.creator@gmail.com>
2021-04-22 16:37:31 +0200

    Update Credentials

--- TheOtherRoles/CredentialsPatch.cs
+++ TheOtherRoles/CredentialsPatch.cs
@@ -10,13 +10,19 @@ namespace TheOtherRoles
 {
     [HarmonyPatch]
     public static class CredentialsPatch {
-        [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
+        public static string fullCredentials = 
+$@"<color=#FCCE03FF>TheOtherRoles</color> v{TheOtherRolesPlugin.Major}.{TheOtherRolesPlugin.Minor}.{TheOtherRolesPlugin.Patch}:
+- Modded by <color=#FCCE03FF>Eisbison</color>,
+  <color=#FFEB91FF>Thunderstorm584</color> and <color=#FFEB91FF>EndOfFile</color>
+- Balanced with <color=#FFEB91FF>Dhalucard</color>
+- Button design by <color=#FFEB91FF>Bavari</color>";
 
+        [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
         private static class VersionShowerPatch
         {
             static void Postfix(VersionShower __instance) {
                 string spacer = new String('\n', 8);
-                string fullCredentials = $"<color=#FCCE03FF>TheOtherRoles</color> v{TheOtherRolesPlugin.Major}.{TheOtherRolesPlugin.Minor}.{TheOtherRolesPlugin.Patch}:\n- Modded by <color=#FCCE03FF>Eisbison</color> and <color=#FFEB91FF>Thunderstorm584</color>\n- Balanced with <color=#FFEB91FF>Dhalucard</color>\n- Button design by <color=#FFEB91FF>Bavari</color>";
+
                 if (__instance.text.text.Contains(spacer))
                     __instance.text.text = __instance.text.text + "\n" + fullCredentials;
                 else
@@ -30,11 +36,13 @@ namespace TheOtherRoles
         {
             static void Postfix(PingTracker __instance)
             {
-                __instance.transform.localPosition = new Vector3(__instance.transform.localPosition.x, 2.675f, __instance.transform.localPosition.z);
-                if (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started)
+                if (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started) {
                     __instance.text.text = "<color=#FCCE03FF>TheOtherRoles</color>\nModded by <color=#FCCE03FF>Eisbison</color>\n" + __instance.text.text;
-                else
-                __instance.text.text = "<color=#FCCE03FF>TheOtherRoles</color>\nModded by <color=#FCCE03FF>Eisbison</color>\nand <color=#FFEB91FF>Thunderstorm584</color>\nBalanced with <color=#FFEB91FF>Dhalucard</color>\nButton design by <color=#FFEB91FF>Bavari</color>\n"  + __instance.text.text;
+                    __instance.transform.localPosition = new Vector3(2.583f, 2.675f, __instance.transform.localPosition.z);
+                } else {
+                    __instance.text.text = $"{fullCredentials}\n{__instance.text.text}";
+                    __instance.transform.localPosition = new Vector3(1.25f, 2.675f, __instance.transform.localPosition.z);
+                }
             }
         }
     }
