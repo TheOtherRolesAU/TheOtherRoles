9f0f55a1ca71bec4c71983c1cfac68c765a53bec
Eisbison <eisbison.creator@gmail.com>
2021-04-23 12:39:51 +0200

    Refactor task count computation

--- TheOtherRoles/Helpers.cs
+++ TheOtherRoles/Helpers.cs
@@ -178,7 +178,7 @@ namespace TheOtherRoles {
         public static bool hasFakeTasks(PlayerControl player) {
             return (player == Jester.jester || player == Jackal.jackal || player == Sidekick.sidekick);
         }
-        
+
         public static string cs(Color c, string s) {
             return string.Format("<color=#{0:X2}{1:X2}{2:X2}{3:X2}>{4}</color>", ToByte(c.r), ToByte(c.g), ToByte(c.b), ToByte(c.a), s);
         }
