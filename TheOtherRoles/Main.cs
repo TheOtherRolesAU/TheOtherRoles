9a38f480b0534d0a440ca14e188d2c978ac70d46
Eisbison <eisbison.creator@gmail.com>
2021-04-26 02:48:29 +0200

    Add multiple buttons

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -31,6 +31,8 @@ namespace TheOtherRoles
 
         public static ConfigEntry<bool> DebugMode { get; private set; }
         public static ConfigEntry<bool> StreamerMode { get; set; }
+        public static ConfigEntry<bool> GhostsSeeTasks { get; set; }
+        public static ConfigEntry<bool> GhostsSeeRoles { get; set; }
         public static ConfigEntry<string> StreamerModeReplacementText { get; set; }
         public static ConfigEntry<string> StreamerModeReplacementColor { get; set; }
         public static ConfigEntry<string> Ip { get; set; }
@@ -49,8 +51,10 @@ namespace TheOtherRoles
         }
 
         public override void Load() {
-            DebugMode  = Config.Bind("Custom", "Enable Debug Mode", false);
-            StreamerMode  = Config.Bind("Custom", "Enable Streamer Mode", false);
+            DebugMode = Config.Bind("Custom", "Enable Debug Mode", false);
+            StreamerMode = Config.Bind("Custom", "Enable Streamer Mode", false);
+            GhostsSeeTasks = Config.Bind("Custom", "Ghosts See Remaining Tasks", true);
+            GhostsSeeRoles = Config.Bind("Custom", "Ghosts See Roles", true);
             StreamerModeReplacementText = Config.Bind("Custom", "Streamer Mode Replacement Text", "\n\nThe Other Roles");
             StreamerModeReplacementColor = Config.Bind("Custom", "Streamer Mode Replacement Text Hex Color", "#87AAF5FF");
             
