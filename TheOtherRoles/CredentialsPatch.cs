6b22a736a0c6cc6306a126f64956c8f01eea4cbf
Eisbison <eisbison.creator@gmail.com>
2021-05-13 15:48:05 +0200

    Update credentials

--- TheOtherRoles/CredentialsPatch.cs
+++ TheOtherRoles/CredentialsPatch.cs
@@ -11,23 +11,35 @@ namespace TheOtherRoles
     [HarmonyPatch]
     public static class CredentialsPatch {
         public static string fullCredentials = 
-$@"<color=#FCCE03FF>TheOtherRoles</color> v{TheOtherRolesPlugin.Version.ToString()}:
-- Modded by <color=#FCCE03FF>Eisbison</color>,
-  <color=#FFEB91FF>Thunderstorm584</color> and <color=#FFEB91FF>EndOfFile</color>
-- Balanced with <color=#FFEB91FF>Dhalucard</color>
-- Button design by <color=#FFEB91FF>Bavari</color>";
+$@"<size=130%><color=#ff351f>TheOtherRoles</color></size> v{TheOtherRolesPlugin.Version.ToString()}:
+- Modded by <color=#FFFF00>Eisbison</color>,
+  <color=#FFFF00>Thunderstorm584</color> & <color=#FFFF00>EndOfFile</color>
+- Balanced with <color=#FFFF00>Dhalucard</color>
+- Button design by <color=#FFFF00>Bavari</color>";
+
+    public static string mainMenuCredentials = 
+$@"Modded by <color=#FFFF00>Eisbison, Thunderstorm584</color> & <color=#FFFF00>EndOfFile</color>
+Balanced with <color=#6B31B9>Dhalucard</color>      Design by <color=#FF5B00>Bavari</color>";
 
         [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
         private static class VersionShowerPatch
         {
             static void Postfix(VersionShower __instance) {
-                string spacer = new String('\n', 8);
+                var amongUsLogo = GameObject.Find("bannerLogo_AmongUs");
+                if (amongUsLogo == null) return;
 
-                if (__instance.text.text.Contains(spacer))
-                    __instance.text.text = __instance.text.text + "\n" + fullCredentials;
-                else
-                    __instance.text.text = __instance.text.text + spacer + fullCredentials;
-                __instance.text.alignment = TMPro.TextAlignmentOptions.TopLeft;
+                var credentials = UnityEngine.Object.Instantiate<TMPro.TextMeshPro>(__instance.text);
+                credentials.transform.position = new Vector3(0, 0.1f, 0);
+                credentials.SetText(mainMenuCredentials);
+                credentials.alignment = TMPro.TextAlignmentOptions.Center;
+                credentials.fontSize *= 0.75f;
+
+                var version = UnityEngine.Object.Instantiate<TMPro.TextMeshPro>(credentials);
+                version.transform.position = new Vector3(0, -0.25f, 0);
+                version.SetText($"v{TheOtherRolesPlugin.Version.ToString()}");
+
+                credentials.transform.SetParent(amongUsLogo.transform);
+                version.transform.SetParent(amongUsLogo.transform);
             }
         }
 
@@ -37,12 +49,29 @@ $@"<color=#FCCE03FF>TheOtherRoles</color> v{TheOtherRolesPlugin.Version.ToString
             static void Postfix(PingTracker __instance)
             {
                 if (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started) {
-                    __instance.text.text = "<color=#FCCE03FF>TheOtherRoles</color>\nModded by <color=#FCCE03FF>Eisbison</color>\n" + __instance.text.text;
+                    __instance.text.text = $"<size=130%><color=#ff351f>TheOtherRoles</color></size> v{TheOtherRolesPlugin.Version.ToString()}\n" + __instance.text.text;
                     __instance.transform.localPosition = new Vector3(2.583f, 2.675f, __instance.transform.localPosition.z);
                 } else {
                     __instance.text.text = $"{fullCredentials}\n{__instance.text.text}";
                     __instance.transform.localPosition = new Vector3(1.25f, 2.675f, __instance.transform.localPosition.z);
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+        private static class LogoPatch
+        {
+            static void Postfix(PingTracker __instance) {
+                var amongUsLogo = GameObject.Find("bannerLogo_AmongUs");
+                if (amongUsLogo != null) {
+                    amongUsLogo.transform.localScale *= 0.6f;
+                    amongUsLogo.transform.position += Vector3.up * 0.25f;
+                }
+
+                var torLogo = new GameObject("bannerLogo_TOR");
+                torLogo.transform.position = Vector3.up;
+                var renderer = torLogo.AddComponent<SpriteRenderer>();
+                renderer.sprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.Banner.png", 300f);                                
             }
         }
     }
