7552bdcc47139341ad2d5029f756fb6196d8df66
Eisbison <eisbison.creator@gmail.com>
2021-05-13 16:10:20 +0200

    Update text size

--- TheOtherRoles/CredentialsPatch.cs
+++ TheOtherRoles/CredentialsPatch.cs
@@ -12,10 +12,10 @@ namespace TheOtherRoles
     public static class CredentialsPatch {
         public static string fullCredentials = 
 $@"<size=130%><color=#ff351f>TheOtherRoles</color></size> v{TheOtherRolesPlugin.Version.ToString()}:
-Modded by <color=#FCCE03FF>Eisbison</color>,
+<size=80%>Modded by <color=#FCCE03FF>Eisbison</color>,
 <color=#FCCE03FF>Thunderstorm584</color> & <color=#FCCE03FF>EndOfFile</color>
 Balanced with <color=#FCCE03FF>Dhalucard</color>
-Button design by <color=#FCCE03FF>Bavari</color>";
+Button design by <color=#FCCE03FF>Bavari</color></size>";
 
     public static string mainMenuCredentials = 
 $@"Modded by <color=#FFFF00>Eisbison, Thunderstorm584</color> & <color=#FFFF00>EndOfFile</color>
