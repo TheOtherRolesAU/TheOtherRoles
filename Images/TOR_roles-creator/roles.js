c871ac72ba6dbffac742bbd4a891548fdd4d33aa
Thomas Kretschmann <t.kretschmann@t-online.de>
2021-08-16 15:26:30 +0200

    restructured code

---
+++ Images/TOR_roles-creator/roles.js
@@ -0,0 +1,182 @@
+const roles = [
+  {
+    name: "Mafioso",
+    desc: "work with the <#FF1919>mafia</> to kill the crewmates",
+    color: "#FF1919",
+  },
+  {
+    name: "Janitor",
+    desc: "work with the <#FF1919>mafia</> by hiding dead bodies",
+    color: "#FF1919",
+  },
+  {
+    name: "Godfather",
+    desc: "kill all crewmates",
+    color: "#FF1919",
+  },
+  {
+    name: "Cleaner",
+    desc: "kill everyone and leave no traces",
+    color: "#FF1919",
+  },
+  {
+    name: "Morphling",
+    desc: "change your look to not get caught",
+    color: "#FF1919",
+  },
+  {
+    name: "Trickster",
+    desc: "use your jack-in-the-boxes to suprise others",
+    color: "#FF1919",
+  },
+  {
+    name: "Camouflager",
+    desc: "camouflage and kill the crewmates",
+    color: "#FF1919",
+  },
+  {
+    name: "Warlock",
+    desc: "curse other players and kill everyone",
+    color: "#FF1919",
+  },
+  {
+    name: "Vampire",
+    desc: "kill the crewmates with your bites",
+    color: "#FF1919",
+  },
+  {
+    name: "Eraser",
+    desc: "kill the crewmates and erase their role",
+    color: "#FF1919",
+  },
+  {
+    name: "Bad Mini",
+    desc: "no one will harm you until you grow up",
+    color: "#FF1919",
+  },
+  {
+    name: "Evil Guesser",
+    desc: "guess and shoot",
+    color: "#FF1919",
+  },
+  {
+    name: "Imp <#FF02C2>Lover</>",
+    desc: "you are in <#FF02C2>love</> with <#FF02C2>eisbison</>",
+    color: "#FF1919",
+  },
+  {
+    name: "Lover",
+    desc: "you are in <#FF02C2>love</> with <#FF02C2>eisbison</>",
+    color: "#FF02C2",
+  },
+  {
+    name: "Jester",
+    desc: "get voted out",
+    color: "#FE58AF",
+  },
+  {
+    name: "Good Mini",
+    desc: "no one will harm you until you grow up",
+    color: "#F8BBDC",
+  },
+  {
+    name: "Swapper",
+    desc: "swap votes to exile the <#FF1919>impostors</>",
+    color: "#9B335E",
+  },
+  {
+    name: "Tracker",
+    desc: "track the <#FF1919>impostors</> down",
+    color: "#7230F0",
+  },
+  {
+    name: "Bait",
+    desc: "force your killer to self report",
+    color: "#7230F0",
+  },
+  {
+    name: "Time Master",
+    desc: "save yourself with your time shield",
+    color: "#6D8FF0",
+  },
+  {
+    name: "Detective",
+    desc: "find the <#FF1919>impostors</> by examining foodprints",
+    color: "#0A6AAA",
+  },
+  {
+    name: "Engineer",
+    desc: "maintain important systems on the ship",
+    color: "#0012FD",
+  },
+  {
+    name: "Jackal",
+    desc: "kill all crewmates and <#FF1919>impostors</> to win",
+    color: "#00BBF4",
+  },
+  {
+    name: "Sidekick",
+    desc: "help your <#00BBF4>jackal</> to kill everyone",
+    color: "#00BBF4",
+  },
+  {
+    name: "Medic",
+    desc: "create a shild to protect a person",
+    color: "#3AFFC9",
+  },
+  {
+    name: "Mayor",
+    desc: "your vote counts twice",
+    color: "#03332B",
+  },
+  {
+    name: "Seer",
+    desc: "you will see players die",
+    color: "#41C16C",
+  },
+  {
+    name: "Hacker",
+    desc: "hack systems to find the <#FF1919>impostors</>",
+    color: "#00F500",
+  },
+  {
+    name: "Snitch",
+    desc: "finish your tasks to find the <#FF1919>impostors</>",
+    color: "#A3FF00",
+  },
+  {
+    name: "Lighter",
+    desc: "your light never goes out",
+    color: "#EBE0B5",
+  },
+  {
+    name: "Shifter",
+    desc: "shift your role",
+    color: "#666666",
+  },
+  {
+    name: "Security Guard",
+    desc: "seal vents and place cameras",
+    color: "#CEB851",
+  },
+  {
+    name: "Nice Guesser",
+    desc: "guess and shoot",
+    color: "#FFFF00",
+  },
+  {
+    name: "Sheriff",
+    desc: "shoot the <#FF1919>imposters</>",
+    color: "#FDC900",
+  },
+  {
+    name: "Arsonist",
+    desc: "let them burn",
+    color: "#FD6700",
+  },
+  {
+    name: "Spy",
+    desc: "confuse the <#FF1919>imposters</>",
+    color: "#FF1919",
+  },
+];
