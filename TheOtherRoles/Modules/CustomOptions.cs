c67dd57769ca0c856b4f9c020db519df5ef4f88d
Mallöris <11Counter11@gmail.com>
2021-11-26 18:51:46 +0100

    gap fix

--- TheOtherRoles/Modules/CustomOptions.cs
+++ TheOtherRoles/Modules/CustomOptions.cs
@@ -391,10 +391,10 @@ namespace TheOtherRoles {
                 gap = 6;
                 index = hudString.TakeWhile(c => (gap -= (c == '\n' ? 1 : 0)) > 0).Count();
                 hudString = hudString.Insert(index, "\n");
-                gap = 19;
+                gap = 20;
                 index = hudString.TakeWhile(c => (gap -= (c == '\n' ? 1 : 0)) > 0).Count();
                 hudString = hudString.Insert(index + 1, "\n");
-                gap = 25;
+                gap = 26;
                 index = hudString.TakeWhile(c => (gap -= (c == '\n' ? 1 : 0)) > 0).Count();
                 hudString = hudString.Insert(index + 1, "\n");
             } else if (counter == 2) {
