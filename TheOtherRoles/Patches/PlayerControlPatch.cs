d68d8049725bba74735cbd51cb364ec80e340b64
Eisbison <eisbison.creator@gmail.com>
2021-11-29 17:17:45 +0100

    Fix vampire teleport on kill

--- TheOtherRoles/Patches/PlayerControlPatch.cs
+++ TheOtherRoles/Patches/PlayerControlPatch.cs
@@ -968,14 +968,11 @@ namespace TheOtherRoles.Patches {
 
     [HarmonyPatch(typeof(KillAnimation), nameof(KillAnimation.CoPerformKill))]
     class KillAnimationCoPerformKillPatch {
+        public static bool hideNextAnimation = true;
         public static void Prefix(KillAnimation __instance, [HarmonyArgument(0)]ref PlayerControl source, [HarmonyArgument(1)]ref PlayerControl target) {
-            if (Vampire.vampire != null && Vampire.vampire == source && Vampire.bitten != null && Vampire.bitten == target)
+            if (hideNextAnimation)
                 source = target;
-            
-            if (Warlock.warlock != null && Warlock.warlock == source && Warlock.curseKillTarget != null && Warlock.curseKillTarget == target) {
-                source = target;
-                Warlock.curseKillTarget = null; // Reset here
-            }
+            hideNextAnimation = false;
         }
     }
 
