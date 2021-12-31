a5be264c2bb80dd93b64433aa05302cb260bcba3
Alex <896956+Alex2911@users.noreply.github.com>
2021-12-31 01:18:47 +0100

    Fixes
    Changed Guesser.guesserCantGuessSnitch

--- TheOtherRoles/Patches/MeetingPatch.cs
+++ TheOtherRoles/Patches/MeetingPatch.cs
@@ -261,7 +261,7 @@ namespace TheOtherRoles.Patches {
                 RoleId guesserRole = (Guesser.niceGuesser != null && PlayerControl.LocalPlayer.PlayerId == Guesser.niceGuesser.PlayerId) ? RoleId.NiceGuesser :  RoleId.EvilGuesser;
                 if (roleInfo.roleId == RoleId.Lover || roleInfo.roleId == guesserRole || roleInfo == RoleInfo.niceMini || (!Guesser.evilGuesserCanGuessSpy && guesserRole == RoleId.EvilGuesser && roleInfo.roleId == RoleId.Spy)) continue; // Not guessable roles
                 
-                if (CustomOptionHolder.guesserCantGuessSnitchIfTaksDone.getBool())
+                if (Guesser.guesserCantGuessSnitch)
                 {
                     var (playerCompleted, playerTotal) = TasksHandler.taskInfo(Snitch.snitch.Data);
                     int numberOfLeftTasks = playerTotal - playerCompleted;
