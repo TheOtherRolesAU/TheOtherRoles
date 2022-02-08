8f156beb0ce0a9752e16f338a15af44d9a680414
gendelo3 <gendelo3@gmail.com>
2022-02-08 10:20:34 +0100

    show ninjas color as part of the trace for trace duration / 4 seconds.

--- TheOtherRoles/Objects/NinjaTrace.cs
+++ TheOtherRoles/Objects/NinjaTrace.cs
@@ -13,7 +13,7 @@ namespace TheOtherRoles.Objects {
         private static Sprite TraceSprite;
         public static Sprite getTraceSprite() {
             if (TraceSprite) return TraceSprite;
-            TraceSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.NinjaTrace.png", 300f);
+            TraceSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.NinjaTraceW.png", 300f);
             return TraceSprite;
         }
 
@@ -27,7 +27,22 @@ namespace TheOtherRoles.Objects {
             traceRenderer.sprite = getTraceSprite();
 
             timeRemaining = duration;
-            
+
+            // display the ninjas color in the trace for duration / 4 seconds, could be an extra setting.
+            HudManager.Instance.StartCoroutine(Effects.Lerp(duration / 4, new Action<float>((p) => {  // time could be fixed as a setting manually etc.
+                Color c = Palette.PlayerColors[(int)Ninja.ninja.Data.DefaultOutfit.ColorId];
+                if (Camouflager.camouflageTimer > 0) {
+                    c = Palette.PlayerColors[6];
+                }
+
+                Color g = Palette.PlayerColors[6];  // default grey
+
+                Color combinedColor = Mathf.Clamp01(p) * g + Mathf.Clamp01(1 - p) * c;
+
+                if (traceRenderer) traceRenderer.color = combinedColor;
+            })));
+
+
             trace.SetActive(true);
             traces.Add(this);
         }
@@ -43,6 +58,8 @@ namespace TheOtherRoles.Objects {
                 if (traceCurrent.timeRemaining < 0)
                 {
                     traceCurrent.trace.SetActive(false);
+                    UnityEngine.Object.Destroy(traceCurrent.trace);
+                    traces.Remove(traceCurrent);
                 }
             }
         }
