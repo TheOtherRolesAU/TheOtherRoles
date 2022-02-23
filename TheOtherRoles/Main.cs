8ca8912e4d5f605aed688aef68b0acaa96d16e36
LaicosVK <vincent.kresse@gmail.com>
2022-02-23 07:13:51 +0100

    Update Main.cs

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -22,7 +22,7 @@ namespace TheOtherRoles
     {
         public const string Id = "me.eisbison.theotherroles";
 
-        public const string VersionString = "3.4.3.1";
+        public const string VersionString = "3.4.3.2";
 
         public static System.Version Version = System.Version.Parse(VersionString);
 
