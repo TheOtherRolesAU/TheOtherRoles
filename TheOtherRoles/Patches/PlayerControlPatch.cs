b9a0cedf34ae1415671b7abd7aeff52a7cbe307f
gendelo3 <gendelo3@gmail.com>
2022-04-05 19:39:49 +0200

    fix KillAnimationCoPerformKillPatch.hideNextAnimation hiding the first kill animation in every game

--- TheOtherRoles/Patches/PlayerControlPatch.cs
+++ TheOtherRoles/Patches/PlayerControlPatch.cs
@@ -1021,7 +1021,7 @@ namespace TheOtherRoles.Patches {
 
     [HarmonyPatch(typeof(KillAnimation), nameof(KillAnimation.CoPerformKill))]
     class KillAnimationCoPerformKillPatch {
-        public static bool hideNextAnimation = true;
+        public static bool hideNextAnimation = false;
         public static void Prefix(KillAnimation __instance, [HarmonyArgument(0)]ref PlayerControl source, [HarmonyArgument(1)]ref PlayerControl target) {
             if (hideNextAnimation)
                 source = target;
