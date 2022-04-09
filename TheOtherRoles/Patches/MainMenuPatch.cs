0366f7f94a85cfacdfdf602c46623eec33481bca
gendelo3 <gendelo3@gmail.com>
2022-04-09 11:14:29 +0200

    Add Horse Mode Button in main menu (remove from mod settings), Add Horse Banner and Switching.

--- TheOtherRoles/Patches/MainMenuPatch.cs
+++ TheOtherRoles/Patches/MainMenuPatch.cs
@@ -1,10 +1,17 @@
 ﻿using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.UI.Button;
+using Object = UnityEngine.Object;
+using TheOtherRoles.Patches;
 
 namespace TheOtherRoles.Modules {
     [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
     public class MainMenuPatch {
+        private static bool horseButtonState = MapOptions.enableHorseMode;
+        private static Sprite horseModeOffSprite = null;
+        private static Sprite horseModeOnSprite = null;
+
         private static void Prefix(MainMenuManager __instance) {
             CustomHatLoader.LaunchHatFetcher();
             var template = GameObject.Find("ExitGameButton");
@@ -32,7 +39,37 @@ namespace TheOtherRoles.Modules {
                 buttonSpriteDiscord.color = textDiscord.color = discordColor;
             });
 
-        }
-    }
 
+            // Horse mode stuff
+            var horseModeSelectionBehavior = new ClientOptionsPatch.SelectionBehaviour("Enable Horse Mode", () => MapOptions.enableHorseMode = TheOtherRolesPlugin.EnableHorseMode.Value = !TheOtherRolesPlugin.EnableHorseMode.Value, TheOtherRolesPlugin.EnableHorseMode.Value);
+
+            var bottomTemplate = GameObject.Find("InventoryButton");
+            if (template == null) return;
+            var horseButton = Object.Instantiate(bottomTemplate, bottomTemplate.transform.parent);
+            var passiveHorseButton = horseButton.GetComponent<PassiveButton>();
+            var spriteHorseButton = horseButton.GetComponent<SpriteRenderer>();
+
+            horseModeOffSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOff.png", 75f);
+            horseModeOnSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOn.png", 75f);
+
+            spriteHorseButton.sprite = horseButtonState ? horseModeOnSprite : horseModeOffSprite;
+
+            passiveHorseButton.OnClick = new ButtonClickedEvent();
+
+            passiveHorseButton.OnClick.AddListener((UnityEngine.Events.UnityAction)delegate {
+                horseButtonState = horseModeSelectionBehavior.OnClick();
+                if (horseButtonState) {
+                    if (horseModeOnSprite == null) horseModeOnSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOn.png", 75f);
+                    spriteHorseButton.sprite = horseModeOnSprite;
+                }
+                else {
+                    if (horseModeOffSprite == null) horseModeOffSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOff.png", 75f);
+                    spriteHorseButton.sprite = horseModeOffSprite;
+                }
+                CredentialsPatch.LogoPatch.updateSprite();
+            });
+        }
+
+
+    }
 }
