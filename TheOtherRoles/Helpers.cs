367bbb06d733d969a5cafda27772f46d61f1902e
Simon Kelly <simonkellly@gmail.com>
2022-04-20 22:22:21 +0100

    Use actual image conversion

--- TheOtherRoles/Helpers.cs
+++ TheOtherRoles/Helpers.cs
@@ -37,7 +37,7 @@ namespace TheOtherRoles {
                 Stream stream = assembly.GetManifestResourceStream(path);
                 var byteTexture = new byte[stream.Length];
                 var read = stream.Read(byteTexture, 0, (int) stream.Length);
-                LoadImage(texture, byteTexture, false);
+                ImageConversion.LoadImage(texture, byteTexture, false);
                 return texture;
             } catch {
                 System.Console.WriteLine("Error loading texture from resources: " + path);
@@ -50,22 +50,13 @@ namespace TheOtherRoles {
                 if (File.Exists(path))     {
                     Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, true);
                     byte[] byteTexture = File.ReadAllBytes(path);
-                    LoadImage(texture, byteTexture, false);
+                    ImageConversion.LoadImage(texture, byteTexture, false);
                     return texture;
                 }
             } catch {
                 System.Console.WriteLine("Error loading texture from disk: " + path);
             }
             return null;
-        }
-
-        internal delegate bool d_LoadImage(IntPtr tex, IntPtr data, bool markNonReadable);
-        internal static d_LoadImage iCall_LoadImage;
-        private static bool LoadImage(Texture2D tex, byte[] data, bool markNonReadable) {
-            if (iCall_LoadImage == null)
-                iCall_LoadImage = IL2CPP.ResolveICall<d_LoadImage>("UnityEngine.ImageConversion::LoadImage");
-            var il2cppArray = (Il2CppStructArray<byte>) data;
-            return iCall_LoadImage.Invoke(tex.Pointer, il2cppArray.Pointer, markNonReadable);
         }
 
         public static PlayerControl playerById(byte id)
