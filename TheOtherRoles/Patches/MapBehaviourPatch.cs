8041cd7a291d9f2f693a33a9c29f27beeac14b04
gendelo3 <gendelo3@gmail.com>
2022-04-29 12:25:55 +0200

    Fix Tiebreaker 9.0 and maybe even map

--- TheOtherRoles/Patches/MapBehaviourPatch.cs
+++ TheOtherRoles/Patches/MapBehaviourPatch.cs
@@ -23,10 +23,37 @@ namespace TheOtherRoles.Patches {
 		}
 
 		[HarmonyPrefix]
-		[HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.Close))]		
-		static bool Prefix2(MapBehaviour __instance) {
-			if (!MeetingHud.Instance) return true;  // Only run in meetings, and then dont enable the hud like it would happen ingame!
-			__instance.gameObject.SetActive(false);
+		[HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.ShowNormalMap))]
+		static bool Prefix3(MapBehaviour __instance) {
+			if (!MeetingHud.Instance || __instance.IsOpen) return true;  // Only run in meetings and when the map is closed
+
+			if (PlayerControl.LocalPlayer.Data.Role.IsImpostor) {
+				__instance.ShowSabotageMap();
+				return false;
+			}
+			PlayerControl.LocalPlayer.SetPlayerMaterialColors(__instance.HerePoint);
+			__instance.GenericShow();
+			__instance.taskOverlay.Show();
+			__instance.ColorControl.SetColor(new Color(0.05f, 0.2f, 1f, 1f));
+			DestroyableSingleton<HudManager>.Instance.SetHudActive(false);
+			return false;
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.ShowSabotageMap))]
+		static bool Prefix4(MapBehaviour __instance) {
+			if (!MeetingHud.Instance || __instance.IsOpen) return true;  // Only run in meetings and when the map is closed
+
+			if (__instance.specialInputHandler != null) {
+				__instance.specialInputHandler.disableVirtualCursor = true;
+			}
+			PlayerControl.LocalPlayer.SetPlayerMaterialColors(__instance.HerePoint);
+			__instance.GenericShow();
+			__instance.infectedOverlay.gameObject.SetActive(true);
+			__instance.ColorControl.SetColor(Palette.ImpostorRed);
+			__instance.taskOverlay.Show();
+			DestroyableSingleton<HudManager>.Instance.SetHudActive(false);
+			ConsoleJoystick.SetMode_Sabotage();
 			return false;
 		}
 
