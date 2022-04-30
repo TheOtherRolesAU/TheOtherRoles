487da756270ebb7dae3d0e050023f5c8aea88fa4
gendelo3 <gendelo3@gmail.com>
2022-04-30 12:13:45 +0200

    Adjust lights patch

--- TheOtherRoles/Patches/ShipStatusPatch.cs
+++ TheOtherRoles/Patches/ShipStatusPatch.cs
@@ -12,8 +12,7 @@ namespace TheOtherRoles.Patches {
     {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CalculateLightRadius))]
-        public static bool Prefix(ref float __result, ShipStatus __instance, [HarmonyArgument(0)] GameData.PlayerInfo player)
-        {
+        public static bool Prefix(ref float __result, ShipStatus __instance, [HarmonyArgument(0)] GameData.PlayerInfo player) {
             if (!__instance.Systems.ContainsKey(SystemTypes.Electrical)) return true;
 
             // If player is a role which has Impostor vision
@@ -21,52 +20,44 @@ namespace TheOtherRoles.Patches {
                 || (Jackal.jackal != null && Jackal.jackal.PlayerId == player.PlayerId && Jackal.hasImpostorVision)
                 || (Sidekick.sidekick != null && Sidekick.sidekick.PlayerId == player.PlayerId && Sidekick.hasImpostorVision)
                 || (Spy.spy != null && Spy.spy.PlayerId == player.PlayerId && Spy.hasImpostorVision)
-                || (Jester.jester != null && Jester.jester.PlayerId == player.PlayerId && Jester.hasImpostorVision))
-            {
+                || (Jester.jester != null && Jester.jester.PlayerId == player.PlayerId && Jester.hasImpostorVision)) {
                 //__result = __instance.MaxLightRadius * PlayerControl.GameOptions.ImpostorLightMod;
                 __result = GetNeutralLightRadius(__instance, true);
                 return false;
             }
-            
+
             // If player is Lighter with ability active
-            if (Lighter.lighter != null && Lighter.lighter.PlayerId == player.PlayerId && Lighter.lighterTimer > 0f)
-            {
-                float unlerped = Mathf.InverseLerp(__instance.MinLightRadius, __instance.MaxLightRadius, GetNeutralLightRadius(__instance, false));
+            if (Lighter.lighter != null && Lighter.lighter.PlayerId == player.PlayerId && Lighter.lighterTimer > 0f) {
+                float unlerped = Mathf.InverseLerp(__instance.MinLightRadius, __instance.MaxLightRadius, GetNeutralLightRadius(__instance, true));
                 __result = Mathf.Lerp(__instance.MaxLightRadius * Lighter.lighterModeLightsOffVision, __instance.MaxLightRadius * Lighter.lighterModeLightsOnVision, unlerped);
-                return false;
             }
-            
+
             // If there is a Trickster with their ability active
-            if (Trickster.trickster != null && Trickster.lightsOutTimer > 0f) 
-            {
+            else if (Trickster.trickster != null && Trickster.lightsOutTimer > 0f) {
                 float lerpValue = 1f;
-                if (Trickster.lightsOutDuration - Trickster.lightsOutTimer < 0.5f)
-                {
+                if (Trickster.lightsOutDuration - Trickster.lightsOutTimer < 0.5f) {
                     lerpValue = Mathf.Clamp01((Trickster.lightsOutDuration - Trickster.lightsOutTimer) * 2);
-                }
-                else if (Trickster.lightsOutTimer < 0.5)
-                {
+                } else if (Trickster.lightsOutTimer < 0.5) {
                     lerpValue = Mathf.Clamp01(Trickster.lightsOutTimer * 2);
                 }
 
                 __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius, 1 - lerpValue) * PlayerControl.GameOptions.CrewLightMod;
-                return false;
             }
 
             // If player is Lawyer, apply Lawyer vision modifier
-            if (Lawyer.lawyer != null && Lawyer.lawyer.PlayerId == player.PlayerId)
-            {
+            else if (Lawyer.lawyer != null && Lawyer.lawyer.PlayerId == player.PlayerId) {
                 float unlerped = Mathf.InverseLerp(__instance.MinLightRadius, __instance.MaxLightRadius, GetNeutralLightRadius(__instance, false));
                 __result = Mathf.Lerp(__instance.MinLightRadius, __instance.MaxLightRadius * Lawyer.vision, unlerped);
                 return false;
             }
 
             // Default light radius
-            __result = GetNeutralLightRadius(__instance, false);
+            else {
+                __result = GetNeutralLightRadius(__instance, false);
+            }
             if (Sunglasses.sunglasses.FindAll(x => x.PlayerId == player.PlayerId).Count > 0) // Sunglasses
-                __result *= 1f - Sunglasses.vision * 0.1f; // TODO! SUNGLASSES MAYBE NOT ALWAYS APPLIED
+                __result *= 1f - Sunglasses.vision * 0.1f;
 
-            
             return false;
         }
 
