f1317aa93d0e79c3c645732cc043aec9b79fc11f
gendelo3 <gendelo3@gmail.com>
2022-05-03 10:39:25 +0200

    Version bump to 4.1.2

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -22,7 +22,7 @@ namespace TheOtherRoles
     public class TheOtherRolesPlugin : BasePlugin
     {
         public const string Id = "me.eisbison.theotherroles";
-        public const string VersionString = "4.1.1";
+        public const string VersionString = "4.1.2";
 
         public static Version Version = Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
