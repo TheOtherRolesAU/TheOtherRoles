29e5098666d40d92b523a2ea11285430ba0af7a8
Jerry <97370685+JustASysAdmin@users.noreply.github.com>
2022-05-18 15:48:28 -0400

    Update CustomOptionHolder.cs

--- TheOtherRoles/CustomOptionHolder.cs
+++ TheOtherRoles/CustomOptionHolder.cs
@@ -272,9 +272,6 @@ namespace TheOtherRoles {
         public static CustomOption allowParallelMedBayScans;
         public static CustomOption shieldFirstKill;
 
-        public static CustomOption showButtonTarget;
-
-
         public static CustomOption dynamicMap;
         public static CustomOption dynamicMapEnableSkeld;
         public static CustomOption dynamicMapEnableMira;
@@ -565,8 +562,6 @@ namespace TheOtherRoles {
             dynamicMapEnableAirShip = CustomOption.Create(504, Types.General, "Enable Airship Rotation", true, dynamicMap, false);
 
             dynamicMapEnableSubmerged = CustomOption.Create(505, Types.General, "Enable Submerged Rotation", true, dynamicMap, false);
-
-            showButtonTarget = CustomOption.Create(9994, Types.General, "Show Button Target", true);
 
 
             blockedRolePairings.Add((byte)RoleId.Vampire, new [] { (byte)RoleId.Warlock});
