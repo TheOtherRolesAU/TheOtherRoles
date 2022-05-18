df74f334ea595042747eb88de844efc3f7687d14
JustASysAdmin <jerry@hosterslice.com>
2022-05-18 18:33:28 -0400

    Fix Dup line + Bump Version

--- TheOtherRoles/CustomOptionHolder.cs
+++ TheOtherRoles/CustomOptionHolder.cs
@@ -576,7 +576,6 @@ namespace TheOtherRoles {
             dynamicMapEnableAirShip = CustomOption.Create(504, Types.General, "Enable Airship Rotation", true, dynamicMap, false);
 
             dynamicMapEnableSubmerged = CustomOption.Create(505, Types.General, "Enable Submerged Rotation", true, dynamicMap, false);
-	    showButtonTarget = CustomOption.Create(9994, Types.General, "Show Button Target", true);
 
             showButtonTarget = CustomOption.Create(9994, Types.General, "Show Button Target", true);
 
