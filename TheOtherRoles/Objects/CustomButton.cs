68c8f37286877734ea1dc841ba82e451cc3bd0a8
Simon Kelly <simonkellly@gmail.com>
2022-05-20 01:05:28 +0100

    Update CustomButton.cs

--- TheOtherRoles/Objects/CustomButton.cs
+++ TheOtherRoles/Objects/CustomButton.cs
@@ -151,8 +151,8 @@ namespace TheOtherRoles.Objects {
 
         public void Update()
         {
-            var localPlayer = CachedPlayer.LocalPlayer.PlayerControl;
-            var moveable = localPlayer.moveable;
+            var localPlayer = CachedPlayer.LocalPlayer;
+            var moveable = localPlayer.PlayerControl.moveable;
             
             if (localPlayer.Data == null || MeetingHud.Instance || ExileController.Instance || !HasButton()) {
                 setActive(false);
@@ -163,7 +163,7 @@ namespace TheOtherRoles.Objects {
             if (DeputyTimer >= 0) { // This had to be reordered, so that the handcuffs do not stop the underlying timers from running
                 if (HasEffect && isEffectActive)
                     DeputyTimer -= Time.deltaTime;
-                else if (!localPlayer.inVent && moveable)
+                else if (!localPlayer.PlayerControl.inVent && moveable)
                     DeputyTimer -= Time.deltaTime;
             }
 
@@ -199,7 +199,7 @@ namespace TheOtherRoles.Objects {
             if (Timer >= 0) {
                 if (HasEffect && isEffectActive)
                     Timer -= Time.deltaTime;
-                else if (!localPlayer.inVent && moveable)
+                else if (!localPlayer.PlayerControl.inVent && moveable)
                     Timer -= Time.deltaTime;
             }
             
