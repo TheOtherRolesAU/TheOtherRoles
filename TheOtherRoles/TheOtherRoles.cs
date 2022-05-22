81f911319c2e24d7f9f2ff49d9d8c61c5055cff4
gendelo3 <gendelo3@gmail.com>
2022-05-22 13:05:50 +0200

    Make sure the anti tp position is synced correctly

--- TheOtherRoles/TheOtherRoles.cs
+++ TheOtherRoles/TheOtherRoles.cs
@@ -1560,7 +1560,7 @@ namespace TheOtherRoles
         public static void setPosition() {
             if (position == Vector3.zero) return;  // Check if this has been set, otherwise first spawn on submerged will fail
             if (antiTeleport.FindAll(x => x.PlayerId == CachedPlayer.LocalPlayer.PlayerId).Count > 0) {
-                CachedPlayer.LocalPlayer.transform.position = position;
+                CachedPlayer.LocalPlayer.NetTransform.RpcSnapTo(position);
                 if (SubmergedCompatibility.IsSubmerged) {
                     SubmergedCompatibility.ChangeFloor(position.y > -7);
                 }
