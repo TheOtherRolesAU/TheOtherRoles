b27667fccaa8000b05772a5de8db5bf59012cca8
Jerry <jerry@hosterslice.com>
2022-05-29 10:48:56 -0500

    Check if lights are off correctly, as well as block name on buttons when Tickster, Lights Sabo, Swooper Swooper, or Camo is active. show morphed player on morphed morphling

--- TheOtherRoles/Buttons.cs
+++ TheOtherRoles/Buttons.cs
@@ -135,7 +135,8 @@ namespace TheOtherRoles
             if (CustomOptionHolder.showButtonTarget.getBool()) { // Should the button show the target name option
                 var text = "";
                 if (Camouflager.camouflageTimer >= 0.1f) text = defaultText; // set text to default if camo is on
-                else if (ShipStatusPatch.lightsOut <= PlayerControl.GameOptions.CrewLightMod) text = defaultText; // set to default if lights are out
+                else if (Helpers.isLightsActive()) text = defaultText; // set to default if lights are out
+                else if (Trickster.trickster != null && Trickster.lightsOutTimer > 0f) text = defaultText; // set to default if trickster ability is active
                 else if (Morphling.morphling != null && Morphling.morphTarget != null && target == Morphling.morphling && Morphling.morphTimer > 0) text = Morphling.morphTarget.Data.PlayerName;  // set to morphed player
                 else if (target == Swooper.swooper && Swooper.isInvisable) text = defaultText;
                 else if (target == null) text = defaultText; // Set text to defaultText if no target
