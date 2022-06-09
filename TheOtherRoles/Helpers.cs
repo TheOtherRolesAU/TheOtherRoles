62f890fe326ef0e0c6c39f9da092aa83c71931bf
Jerry <jerry@hosterslice.com>
2022-06-09 18:48:58 -0500

    Move button text function to helpers

--- TheOtherRoles/Helpers.cs
+++ TheOtherRoles/Helpers.cs
@@ -134,6 +134,29 @@ namespace TheOtherRoles {
             }
         }
 
+        public static void showTargetNameOnButton(PlayerControl target, CustomButton button, string defaultText) {
+            if (CustomOptionHolder.showButtonTarget.getBool()) { // Should the button show the target name option
+                var text = "";
+                if (Camouflager.camouflageTimer >= 0.1f) text = defaultText; // set text to default if camo is on
+                else if (Helpers.isLightsActive()) text = defaultText; // set to default if lights are out
+                else if (Trickster.trickster != null && Trickster.lightsOutTimer > 0f) text = defaultText; // set to default if trickster ability is active
+                else if (Morphling.morphling != null && Morphling.morphTarget != null && target == Morphling.morphling && Morphling.morphTimer > 0) text = Morphling.morphTarget.Data.PlayerName;  // set to morphed player
+                else if (target == Swooper.swooper && Swooper.isInvisable) text = defaultText;
+                else if (target == null) text = defaultText; // Set text to defaultText if no target
+                else text = target.Data.PlayerName; // Set text to playername
+                showTargetNameOnButtonExplicit(null, button, text);
+            }
+        }
+
+
+        public static void showTargetNameOnButtonExplicit(PlayerControl target, CustomButton button, string defaultText) {
+            var text = defaultText;
+            if (target == null) text = defaultText; // Set text to defaultText if no target
+            else text = target.Data.PlayerName; // Set text to playername
+            button.actionButton.OverrideText(text);
+            button.showButtonText = true;
+        }
+
 
         public static Sprite loadSpriteFromResources(string path, float pixelsPerUnit) {
             try
