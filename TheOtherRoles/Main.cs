d517c3095627c00595976d8426fc8b626f5914c4
Jerry <jerry@hosterslice.com>
2022-07-03 20:02:39 -0500

    Push Ver

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -23,7 +23,7 @@ namespace TheOtherRoles
     public class TheOtherRolesPlugin : BasePlugin
     {
         public const string Id = "me.eisbison.theotherroles";
-        public const string VersionString = "5.4.4";
+        public const string VersionString = "5.4.5";
 
         public static Version Version = Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
