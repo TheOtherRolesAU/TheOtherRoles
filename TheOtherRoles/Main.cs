3d0f97c65a92a4916f9eed2ed5424d717578537e
Jerry <jerry@hosterslice.com>
2022-07-18 11:49:31 -0500

    Update Version

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -23,7 +23,7 @@ namespace TheOtherRoles
     public class TheOtherRolesPlugin : BasePlugin
     {
         public const string Id = "me.eisbison.theotherroles";
-        public const string VersionString = "5.4.8";
+        public const string VersionString = "5.4.9";
         public static Version Version = Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
 
