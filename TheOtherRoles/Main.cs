4a109801e2372215cc8218b83e333d2268e6c33d
Jerry <jerry@hosterslice.com>
2022-08-20 19:30:26 -0500

    Revert to 5.6.0

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -23,7 +23,7 @@ namespace TheOtherRoles
     public class TheOtherRolesPlugin : BasePlugin
     {
         public const string Id = "me.eisbison.theotherroles";
-        public const string VersionString = "5.6.0";
+        public const string VersionString = "5.6.2";
         public static Version Version = Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
 
